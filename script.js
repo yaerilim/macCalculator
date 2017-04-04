14ed168b4f3b693feba6a2209921a221743455ce
Yaeri Lim <yaerilim@gmail.com>
2017-04-04 15:54:14 -0700

    added css effect when key pressed and media query

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
     $('body').keypress(key_display);
     $('body').keyup(function(event){               //esc key works on 'keyup' not 'keypress'
         if(event.which === 27){
+            active_css($('.clear'));
             clear();
         }
     });
@@ -16,6 +17,13 @@ $(document).ready(function() {
 //---------------------------------- KEY SOUND ----------------------------------
 function audio_on(){
     $('.sound').trigger('play');
+}
+//---------------------------------- KEY ACTIVE CSS EFFECT ----------------------------------
+function active_css(key_number){
+    key_number.addClass('active');
+    setTimeout(function(){
+        key_number.removeClass('active');
+    }, 100);
 }
 //---------------------------------- WHEN CLEAR BUTTON CLICKED ----------------------------------
 function clear() {
@@ -33,51 +41,67 @@ function key_display(){                 //taking keyboard input (keycode event)
     switch(event.which) {
         case 48:
             value = 0;
+            active_css($('.zero'));
             break;
         case 49:
             value = 1;
+            active_css($('.one'));
             break;
         case 50:
             value = 2;
+            active_css($('.two'));
             break;
         case 51:
             value = 3;
+            active_css($('.three'));
             break;
         case 52:
             value = 4;
+            active_css($('.four'));
             break;
         case 53:
             value = 5;
+            active_css($('.five'));
             break;
         case 54:
             value = 6;
+            active_css($('.six'));
             break;
         case 55:
             value = 7;
+            active_css($('.seven'));
             break;
         case 56:
             value = 8;
+            active_css($('.eight'));
             break;
         case 57:
             value = 9;
+            active_css($('.nine'));
             break;
         case 46:
             value = '.';
+            active_css($('.decimal'));
             break;
         case 43:
             value = '+';
+            active_css($('.plus'));
             break;
         case 45:
             value = '-';
+            active_css($('.minus'));
             break;
         case 42:
             value = '*';
+            active_css($('.multiply'));
             break;
         case 47:
             value = '/';
+            active_css($('.divide'));
             break;
         case 13:
             value = '=';
+            active_css($('.equal'));
             calculation();
             break;
     }
@@ -126,8 +150,11 @@ function calculation() {
     $('.display').val(order_of_operation);
     var last_value = key_input_arr[0];
     display_arr = [];
-    if(isNaN(last_value)){
+    if(isNaN(last_value) || isFinite(last_value) === false){       //if the result is NaN or Infinity, change to ERROR and run clear()
         display_arr.push("ERROR");
+        setTimeout(function(){
+            clear();
+        },1500)
     }else{
         display_arr.push(last_value);
     }
@@ -163,19 +190,15 @@ function order_of_operation(){
             }
         }
     }
-    if(isFinite(key_input_arr[0])===false){          //division by zero = change infinity to error
-        return "ERROR"
-    }else{
         return key_input_arr[0];
-    }
 }
 //---------------------------------- CHANGE THE FONT SIZE ON DISPLAY AREA ACCORDING TO THE LENGTH ----------------------------------
 function change_font_size(){
-    if(display_arr.length>8 || display_arr[0].length>8){
+    if(display_arr.length>8 || (display_arr[0].toString().length + display_arr.length)>9){
         $('.display').css('font-size', '2vh')
-    }if(display_arr.length>14 || display_arr[0].length>14){
+    }if(display_arr.length>14 || (display_arr[0].toString().length + display_arr.length)>15){
         $('.display').css('font-size', '1.5vh')
-    }if(display_arr.length>18 || display_arr[0].length>18){
+    }if(display_arr.length>18 || (display_arr[0].toString().length + display_arr.length)>19){
         $('.display').css('font-size', '1vh')
     }
 }
